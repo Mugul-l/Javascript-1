f2a1127e5e9849b7b57aa61a96c7443ef3c284fb
Smruti Pragyan Biswal <83799897+Smrutipragyan@users.noreply.github.com>
2023-12-21 05:47:23 +0000

    conversion

---
+++ conversion.js
@@ -0,0 +1,16 @@
+let num=20;
+console.log(typeof num);
+let num2=String(num);
+console.log( typeof num2);
+
+let Str="SmrutiPragyan";
+let name=Boolean(Str);
+console.log(typeof name); //Boolean
+
+const score=40;
+const scr2=String(score);
+console.log(typeof scr2); //String
+
+let one=1;
+let oneNum=Boolean(one);
+console.log(oneNum);   //true
