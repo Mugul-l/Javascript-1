26aae5f440f2a04e5aa419cb6b70ffe1e3907bc9
Smruti Pragyan Biswal <83799897+Smrutipragyan@users.noreply.github.com>
2023-12-21 05:27:46 +0000

    dataTypes

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-console.log("Smruti Pragyan Biswal");
+// console.log("Smruti Pragyan Biswal");
+const id=30;
+let name="Smruti";
+var address="Cuttack";
+ Phone="826364800";
+let State;
+console.table([id,name,address,Phone,State]);
