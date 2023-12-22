d3435933f1806fefc572666d85c485a47b9e14e9
Smruti Pragyan Biswal <83799897+Smrutipragyan@users.noreply.github.com>
2023-12-22 16:36:45 +0000

    Strings and methods

---
+++ String.js
@@ -0,0 +1,23 @@
+// let str=("My name is"+varname+"and I am a student.")- old method
+//New method
+let name=" Smruti Pragyan Biswal ";
+console.log(`My name is ${name}`); //This is called String 'interpolation'.
+
+//We can declare string in another way as:
+let str=new String("Anshuman");
+console.log(str);    //[String: 'Anshuman']
+console.log(str[2]);    // s
+console.log(str.length);    //8
+console.log(str.concat( " Biswal"));    //Anshuman Biswal
+console.log(str.toUpperCase()); //ANSHUMAN
+console.log(str.charAt(4)); // u
+console.log(str.indexOf('h'));  //3
+console.log(str.substring(0,5));    //Anshu
+console.log(str.slice(-6,5));  //shu
+console.log(name.trim());   //will remove the white spaces present in the whole string
+const url="https://smrutipragyan%15Biswal.com";
+console.log(url.replace('%15','-'));   // https://smrutipragyan-Biswal.com ('%15' replaced with '-')
+console.log(name.split(' '));
+// Many more methods are there...
+
+  console.log(str.__proto__); //  in console we can find the methods in detail by this syntax
