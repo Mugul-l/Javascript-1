22284ecdbc5c8de050cae48125a8fe4f4ca5a956
Smruti Pragyan Biswal <83799897+Smrutipragyan@users.noreply.github.com>
2023-12-23 18:19:10 +0000

    Numbers and Math

---
+++ Numbers_and_Maths.js
@@ -0,0 +1,27 @@
+let number=200;
+console.log(number);    //200
+
+let number2=new Number(5000);
+console.log(number2);   //[Number: 500]
+
+console.log(number.toString()); //200
+console.log(number.toString().length); //3
+console.log(number.toFixed(4)); // 200.0000  it will add the decimal values as'0' accn to the number given by you
+const number3=3.2895;
+console.log(number3.toPrecision(3));    //3.29
+let number4=1000000;
+console.log(number4.toLocaleString('en-IN'));
+
+//          MATHS
+let num=234;
+console.log(Math.sqrt(num));      //  15.297058540778355
+console.log(Math.abs(-87));        //87
+console.log(Math.pow(4,3));         //64
+//Many more...
+
+
+
+
+
+
+
