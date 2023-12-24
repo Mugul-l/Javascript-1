f43d011fcecdc1b886b94b1010101a77fe6be360
Smruti Pragyan Biswal <83799897+Smrutipragyan@users.noreply.github.com>
2023-12-24 07:12:04 +0000

    Numbers and Maths

--- Numbers_and_Maths.js
+++ Numbers_and_Maths.js
@@ -17,6 +17,11 @@ let num=234;
 console.log(Math.sqrt(num));      //  15.297058540778355
 console.log(Math.abs(-87));        //87
 console.log(Math.pow(4,3));         //64
+console.log(Math.min(2,4,6,1,8,9)); //1
+console.log(Math.ceil(3.8));        //4
+console.log(Math.floor(3.8));       //3
+console.log(Math.random());     //random value in between 0-1 (decimal number)
+//console.log((Math.random()*10)+1);  // By multiplying 10 we can shift one digit to the left decimal point which wont be in between 0-1 but more then 1
 //Many more...
 
 
