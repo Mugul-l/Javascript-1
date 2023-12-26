73e67c4f3549a83681a58859294e4c716408b2a8
Smruti Pragyan Biswal <83799897+Smrutipragyan@users.noreply.github.com>
2023-12-26 15:21:59 +0000

    Objects

---
+++ Object.js
@@ -0,0 +1,81 @@
+//Object Literals
+// let symbol= Symbol("smru123");
+// let obj={
+//     name:"Smruti",
+//     age: 22,
+//     email: "smruti987@gmail.com",
+//     address: "Cuttack",
+//     "occupation": "Engineer",
+//     [symbol]:"smru321"   //we should declare symbol inside [] 
+// }
+
+// console.log(obj.email);
+// //another way to access object
+// console.log(obj["name"]);
+// console.log(obj["occupation"]);
+// //Symbol
+//  console.log(obj[symbol]);
+
+//  obj.age= 30;
+//  console.log(obj.age);
+
+//To Lock the changes 
+//Object.freeze(obj)
+// obj.address="Bhubaneswar";
+// console.log(obj.address); //Cuttack -cuz the obj is freezed
+
+// obj.newUser= function(){
+//     console.log("My Fullname is Smruti Pragyan Biswal");
+// };
+// console.log(obj.newUser()); 
+
+// obj.newUser2= function(){
+//     console.log( `My age is ${this.age} .`);
+// }
+// console.log(obj.newUser2()); 
+
+  //Singleton 
+let User1= new Object() //singleton object
+let User2={} //also an obj but not singleton
+
+User2.name="Anshuman Biswal"
+User2.age=22
+User2.occupation="Engineer"
+console.log(User2);         //{ name: 'Anshuman Biswal', age: 22, occupation: 'Engineer' }
+
+let User3 ={
+    User:{
+        Fullname:{
+            Firstname:"Smruti",
+            Lastname : "Biswal"     //nested function
+        }
+
+    }
+}
+console.log(User3.User.Fullname);
+
+//Add Object 
+let obj1 = {"1":"a","2":"b"}
+let obj2 = {"3":"c","4":"d"}
+
+//let obj3 = {obj1,obj2}     //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+
+//let obj3 = Object.assign({},obj1,obj2)      //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+//give {} so that it will first create an empty string then inside that empty string the rest strings will be added
+
+let obj3= {...obj1,...obj2};    //Spread method like array
+console.log(obj3);
+
+//Array of Objects
+
+let obj4=[
+    {
+        name:"smruti",
+        age:20
+    },
+    {
+        name:"Akhil",
+        age:20
+    }
+]
+console.log(obj4[2].name);
