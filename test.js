69dffe3804017342c5aadd344baad8ee59f31516
Mugul_56 <83799897+Mugul-l@users.noreply.github.com>
2025-07-31 06:45:28 +0000

    State added

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 // console.log("Smruti Pragyan Biswal");
 const id=30;
 let name="Smruti";
-var address="Cuttack";
+var address="Jagatpur";
  Phone="826364800";
-let State;
+let State = "Odisha";
+let Country;
 console.table([id,name,address,Phone,State]);
