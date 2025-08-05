8bd50c3a2ed7bc644e3599941d1386b655c7f353
Mugul_56 <83799897+Mugul-l@users.noreply.github.com>
2025-08-05 06:09:47 +0000

    table learned

--- test.js
+++ test.js
@@ -4,5 +4,5 @@ let name="Smruti";
 var address="Jagatpur";
  Phone="826364800";
 let State = "Odisha";
-let Country;
-console.table([id,name,address,Phone,State]);
+let Country="India";
+console.table([id,name,address,Phone,State,Country]);
