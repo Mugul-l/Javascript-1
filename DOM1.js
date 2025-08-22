80bbe012ec7e340f0fde43b337b004bde12fccfb
Mugul_56 <83799897+Mugul-l@users.noreply.github.com>
2025-08-22 07:36:50 +0000

    Dom

--- DOM1.js
+++ DOM1.js
@@ -1,4 +1,6 @@
 //  What is the DOM?
-// DOM = Document Object Model
+   
+// DOM = Document Object Model--> browser’s representation of HTML.
 // When a web page loads, the browser creates a tree-like structure of the page called the DOM.
-// Each HTML element becomes an object (a node) inside this tree, which JavaScript can access and manipulate.
+// Each HTML element becomes an object (a node) inside this tree, which JavaScript can access and manipulate. Basically Works like a tree of nodes (document → html → head/body → elements).
+
