9c3353628ce34823548b17dd0bd9475c2bb9d3ca
Mugul_56 <83799897+Mugul-l@users.noreply.github.com>
2025-08-23 10:47:19 +0000

    Learned DOM Manipulation

--- DOM1.js
+++ DOM1.js
@@ -7,7 +7,7 @@
 
 //1. getElementBy...   -->  These are the old-school methods in JavaScript. They are specific and give you HTML elements directly.
 
-// getElementById → returns just one element.
+// getElementById → returns just one element. (cuz id is unique)
 // getElementsByClassName / getElementsByTagName → return a list of elements (not a single one).
 
 document.getElementById('Id-name')
@@ -25,3 +25,19 @@ document.querySelector('tag-name')
 document.querySelectorAll('#Id-name')
 document.querySelectorAll('.class-name')
 document.querySelectorAll('tag-name') 
+
+//querySelector → one element (first match).
+//querySelectorAll → all elements (NodeList).  to do the dom manipulation in queryselectprall we need to pass the index as it returns the nodelist. like:- lists[0].style.bgcolor='red'
+// For nodelist there is no map() function to iterate through the whole list but there is Foreach to do the same. But if you want to use map() we have to convert it to array first then we can use.
+// Ex-          lists.forEach((lst)=>{
+//              lst.style.backgroundColor='blue'
+//                   }   )
+
+//get attribute/SetAttribute
+document.getElementById('Id-name').getAttribute('attribute-name')
+document.getElementById('Id-name').setAttribute('attribute-name','set-new-name')
+
+//To access inner contents---
+id.innerText //Returns the visible text (what the user sees) .Ignores HTML tags and hidden elements (display:none, visibility:hidden).Triggers reflow (calculates CSS styles), so it’s slower than textContent.
+id.innerHTML //Returns or sets the HTML content of an element (including tags).
+id.textContent //Returns all text inside the element, regardless of CSS.Does not parse HTML, just gives raw text.Much faster than innerText (no reflow).
